b75e9687ff99e28145150d5c4b9b9ed3a7f3895c
nicholas <nickcts@hotmail.com>
2024-08-15 14:08:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+
+  function fetchBooks() {
+    return fetch("https://anapioficeandfire.com/api/books")
+      .then(response => response.json()) // Note: Use 'response' with lowercase 'r'
+      .then(data => {
+        renderBooks(data);
+      })
+      .catch(error => {
+        console.error('Error fetching books:', error);
+      });
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
